cc3c5bdecb592b2dba78067a79feec606a647827
HTsuruo <tsuru.dev@gmail.com>
2020-09-12 00:19:55 +0900

    json_codecを使ったパターン

--- lib/json_codec_page.dart
+++ lib/json_codec_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class JsonCodecPage extends StatelessWidget {
@@ -7,9 +9,36 @@ class JsonCodecPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final data = DefaultAssetBundle.of(context).loadString('data.json');
     return Scaffold(
       appBar: AppBar(title: const Text(routeName)),
-      body: Container(),
+      body: FutureBuilder<String>(
+        future: data,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final data = snapshot.data;
+          final json = jsonDecode(data) as Map<String, dynamic>;
+          final users = json['users'] as List<dynamic>;
+          return ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              final user = users[index] as Map<String, dynamic>;
+              final id = user['id'] as String;
+              final name = user['name'] as String;
+              final age = user['age'] as int;
+              return ListTile(
+                title: Text(name),
+                subtitle: Text(id),
+                trailing: Text(age.toString()),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
